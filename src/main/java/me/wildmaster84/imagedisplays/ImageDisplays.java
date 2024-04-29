4bdd4659988e7212176136d5713a1bbf87f042f8
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-04-28 23:44:23 -0400

    Added logic into MoviePlayer object

--- src/main/java/me/wildmaster84/imagedisplays/ImageDisplays.java
+++ src/main/java/me/wildmaster84/imagedisplays/ImageDisplays.java
@@ -1,72 +1,16 @@
 package me.wildmaster84.imagedisplays;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.TextDisplay;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
 public class ImageDisplays extends JavaPlugin {
 	
 	public void onEnable() {
         Location loc = new Location(Bukkit.getWorld("world"), -1, 109, 39);
-        loadImage(loc, new File(this.getDataFolder(), "image.png"));
-	}
-	
-	
-	
-	public void loadImage(Location loc, File imagePath) {
-        try {
-            // Load the image
-            BufferedImage image = ImageIO.read(imagePath);
-
-            // Get image width and height
-            double width = image.getWidth();
-            double height = image.getHeight();
-
-            // Access pixels and extract color information
-            for (double y = 0.0; y < height; y++) {
-                for (double x = 0.0; x < width; x++) {
-                    // Get RGB value of the pixel
-                    int rgba = image.getRGB((int)x, (int)y);
-                    
-                    double locX = loc.getX();
-                    
-                    // offsets the image to hardcoded height
-                    double locY = loc.getY()+0.569;
-                    
-                    // Aligns the pixels
-                    if (x != 0) locX = locX+x-(0.9751*x);
-                    if (y != 0) locY = locY-y+(0.9751*y);
-                    
-                    // Sets the 0-0 for X and Y
-                    if (x == 0) locX = locX+x;
-                    if (y == 0) locY = locY+y;
-                    
-                    Location loc2 = new Location(loc.getWorld(), ((double)locX), ((double)locY), loc.getZ());
-                    
-                    // Convert RGB to Color object
-                    Color color = new Color(rgba, true); // true to include alpha
-                    TextDisplay display = (TextDisplay) Bukkit.getWorld("world").spawnEntity(loc2, EntityType.TEXT_DISPLAY);
-                    // Makes a 1x1 pixel
-                    display.setText("");
-                    
-                    
-                    // Sets the pixel color
-                    display.setBackgroundColor(org.bukkit.Color.fromARGB(color.getAlpha(), color.getRed(), color.getGreen(), color.getBlue()));
-                    
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new MoviePlayer(loc).display(new File(this.getDataFolder(), "image.png"));
 	}
 
 }
---
+++ src/main/java/me/wildmaster84/imagedisplays/MoviePlayer.java
@@ -0,0 +1,121 @@
+package me.wildmaster84.imagedisplays;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.TextDisplay;
+import org.bukkit.util.Transformation;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.joml.AxisAngle4f;
+import org.joml.Vector3f;
+
+import me.wildmaster84.imagedisplays.entities.Projector;
+
+public class MoviePlayer implements Projector {
+	private Image pojectorImage;
+	private Location location;
+	private ArrayList<TextDisplay> displays;
+	
+	public MoviePlayer(Location loc) {
+		location = loc;
+		displays = new ArrayList<>();
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		for (TextDisplay textDisplay : displays) {
+			textDisplay.remove();
+			displays.remove(textDisplay);
+		}
+	}
+
+	@Override
+	public void display(@NotNull Image t) {
+		// TODO Auto-generated method stub
+		pojectorImage = t;
+		
+	}
+	
+	public void display(@NotNull File file) {
+		// TODO Auto-generated method stub
+		try {
+			pojectorImage = ImageIO.read(file);
+			loadImage();
+		} catch (IOException e) {
+			Bukkit.getLogger().warning("File not found: " + file.getPath());
+		}
+		
+	}
+	
+
+	@Override
+	public @Nullable Image getDisplay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void loadImage() {		
+		BufferedImage image = flipImageVertical((BufferedImage) pojectorImage);
+		double width = image.getWidth();
+        double height = image.getHeight();
+        
+        Vector3f pixleSize;
+        for (double y = height - 1; y >= 0; y--) {
+            for (double x = 0; x < width; x++) {
+            	int rgba = image.getRGB((int)x, (int)y);
+                Color color = new Color(rgba, true);
+                if (color.getAlpha() != 0) {
+                	double locX = location.getX();                    
+                    double locY = location.getY();
+                    
+                    if (x != 0) locX = locX+x-(0.9844*x);
+                    if (y != 0) locY = locY+y-(0.9844*y);
+                    
+                    if (x == 0) locX = locX+x;
+                    if (y == 0) locY = locY+y;
+                    
+                    Location loc2 = new Location(location.getWorld(), locX, locY, location.getZ());
+                    TextDisplay display = (TextDisplay) loc2.getWorld().spawnEntity(loc2, EntityType.TEXT_DISPLAY);
+                	displays.add(display);
+                    
+                    display.setText("");                    
+                    pixleSize = new Vector3f(0.624f, 0.624f, 0);
+                    display.setTransformation(new Transformation(new Vector3f(), new AxisAngle4f(), pixleSize, new AxisAngle4f()));
+                    display.setBackgroundColor(org.bukkit.Color.fromARGB(color.getAlpha(), color.getRed(), color.getGreen(), color.getBlue()));
+                    
+                }
+        	}
+        }
+	}
+
+	private static BufferedImage flipImageVertical(BufferedImage image) {
+	    // Create a new BufferedImage for the flipped image
+	    BufferedImage flippedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+	
+	    // Create AffineTransform for flipping vertically
+	    AffineTransform tx = AffineTransform.getScaleInstance(1, -1);
+	    tx.translate(0, -image.getHeight());
+	
+	    // Create Graphics2D object to draw the flipped image
+	    Graphics2D g2d = flippedImage.createGraphics();
+	    g2d.drawImage(image, tx, null);
+	    g2d.dispose();
+	
+	    return flippedImage;
+	}
+
+
+}
