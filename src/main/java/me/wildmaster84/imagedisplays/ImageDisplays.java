6e9236a3182252a05066793822b4c96aee39be4b
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-04-29 03:17:34 -0400

    Added logic for TextPlayer object

--- src/main/java/me/wildmaster84/imagedisplays/ImageDisplays.java
+++ src/main/java/me/wildmaster84/imagedisplays/ImageDisplays.java
@@ -7,10 +7,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
 public class ImageDisplays extends JavaPlugin {
+	MoviePlayer moviePlayer;
+	TextPlayer textPlayer;
 	
+	@Override
 	public void onEnable() {
         Location loc = new Location(Bukkit.getWorld("world"), -1, 109, 39);
-        new MoviePlayer(loc).display(new File(this.getDataFolder(), "image.png"));
+        //moviePlayer = new MoviePlayer(loc);
+        //moviePlayer.display(new File(this.getDataFolder(), "image.png"));
+        //textPlayer = new TextPlayer(loc);
+        //textPlayer.display(new File(this.getDataFolder(), "image.png"));
+	}
+	
+	@Override
+	public void onDisable() {
+		//moviePlayer.clear();
+		//textPlayer.clear();
 	}
 
 }
