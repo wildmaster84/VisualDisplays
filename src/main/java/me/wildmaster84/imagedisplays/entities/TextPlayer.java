1dc9639fe47305a5525d051d308c2d1535a05f42
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-04-30 18:12:53 -0400

    Added scaling/GIF looping and removed StringBuilder

--- src/main/java/me/wildmaster84/imagedisplays/entities/TextPlayer.java
+++ src/main/java/me/wildmaster84/imagedisplays/entities/TextPlayer.java
@@ -2,6 +2,7 @@ package me.wildmaster84.imagedisplays.entities;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -22,20 +23,24 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.TextDisplay;
 import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.joml.AxisAngle4f;
 import org.joml.Vector3f;
 
 import me.wildmaster84.imagedisplays.ImageDisplays;
+import me.wildmaster84.imagedisplays.entities.projector.Projector;
 import net.md_5.bungee.api.ChatColor;
 
-public class TextPlayer implements VisualDisplay<String> {
+public class TextPlayer implements Projector {
 	private Location location;
 	private ArrayList<TextDisplay> displays;
 	int currentFrame = 0;
+	List<String> pixels;
 	
 	public TextPlayer(Location loc) {
 		location = loc;
 		displays = new ArrayList<>();
+		pixels = new ArrayList<>();
 	}
 	
 	@Override
@@ -48,49 +53,64 @@ public class TextPlayer implements VisualDisplay<String> {
 	}
 
 	@Override
-	public void display(@NotNull String t) {
+	public void display(@NotNull Image t) {
 		// TODO Auto-generated method stub
 	}
 	
+	
+	// Ensures the image isnt a GIF
+	// No point in looping a static image
+	public void display(@NotNull File file, boolean loop) {
+		if (getFileExtension(file.getPath()).equals("gif")) {
+			playGIF(file, loop);
+		} else {
+			loadImage(file, 64, 64);
+		}
+		
+	}
+	
+	// Ensures the GIF plays regardless
 	public void display(@NotNull File file) {
 		if (getFileExtension(file.getPath()).equals("gif")) {
-			playGIF(file);
+			display(file, false);
 		} else {
-			loadImage(file);
+			loadImage(file, 64, 64);
 		}
 		
 	}
 	
 	
-	private void loadImage(File file) {		
+	private void loadImage(File file, int scaleX, int scaleY) {		
 		BufferedImage image;
-		StringBuilder sb = new StringBuilder();
+		pixels.clear();
 		try {
 			image = flipImageVertical(ImageIO.read(file));
 			double width = image.getWidth();
 	        double height = image.getHeight();
 
-	        TextDisplay display = (TextDisplay) location.getWorld().spawnEntity(location, EntityType.TEXT_DISPLAY);
+	        TextDisplay display;
+	        if (getEntityAtLocation(location) == null) {
+	        	display = (TextDisplay) location.getWorld().spawnEntity(location, EntityType.TEXT_DISPLAY);
+	        } else {
+	        	display = (TextDisplay) getEntityAtLocation(location);
+	        }
         	
 	        for (double y = height - 1; y >= 0; y--) {
 	            for (double x = 0; x < width; x++) {
 	            	int rgba = image.getRGB((int)x, (int)y);
 	                Color color = new Color(rgba, true);
-	                if (sb.toString().length() > 69678*4) break;
+	                if (pixels.size() > 69678*4) break;
 	                
-	                sb.append(ChatColor.of(color)).append(ChatColor.BOLD).append("█");
-	                if ((int)x == (int)width-1) {
-	                	sb.append("\n");
-	                }
+	                pixels.add(ChatColor.of(color) + "" + ChatColor.BOLD + "█" + ((int)x == (int)width-1 && (int)y != 0 ? "\n" : ""));
 	                
 	        	}
 	        }
-	        display.setLineWidth(sb.toString().split("\n")[0].length());
+	        display.setLineWidth(String.join("", pixels).split("\n")[0].length());
 	        
 	        // This should be split into separated lines
-	        display.setText(sb.toString());
+	        display.setText(String.join("", pixels).toString());
 	        
-            display.setTransformation(new Transformation(new Vector3f(0.0078f*(int)width, 0, 0), new AxisAngle4f(), new Vector3f((0.0615f/(int)width)*(int)width, (0.0615f/(int)width)*(int)height, 0), new AxisAngle4f()));
+            display.setTransformation(new Transformation(new Vector3f(0.0078f*(int)scaleX, 0, 0), new AxisAngle4f(), new Vector3f((0.0615f/(int)width)*(int)scaleX, (0.0615f/(int)height)*(int)scaleY, 0), new AxisAngle4f()));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -98,7 +118,7 @@ public class TextPlayer implements VisualDisplay<String> {
 		
 	}
 	
-	private void playGIF(File file) {
+	private void playGIF(File file, boolean loop) {
 		try {
 			FileImageInputStream inputStream = new FileImageInputStream(file);
 			if (!ImageIO.getImageReaders(inputStream).hasNext()) {
@@ -116,9 +136,12 @@ public class TextPlayer implements VisualDisplay<String> {
 					currentFrame++;
 				}
 				if (currentFrame >= frames) {
-					//task.cancel();
-					//return;
-					currentFrame = 0;
+					if (!loop) {
+						task.cancel();
+						return;
+					} else {
+						currentFrame = 0;
+					}
 				}
 
 			}, 1L, 1L);
@@ -129,8 +152,8 @@ public class TextPlayer implements VisualDisplay<String> {
 	
 	private void frame(ImageReader reader, int frameindex) {		
 		BufferedImage image;
+		pixels.clear();
 		try {
-			List<String> pixels = new ArrayList<>();
 			image = flipImageVertical(reader.read(frameindex));
 			double width = image.getWidth();
 	        double height = image.getHeight();
@@ -148,7 +171,7 @@ public class TextPlayer implements VisualDisplay<String> {
 	                
 	                if (pixels.size() > 69678*4) break;
 	                
-	                pixels.add(ChatColor.of(color) + "" + ChatColor.BOLD + "█" + ((int)x == (int)width-1 ? "\n" : ""));
+	                pixels.add(ChatColor.of(color) + "" + ChatColor.BOLD + "█" + ((int)x == (int)width-1 && (int)y != 0 ? "\n" : ""));
 	                
 	        	}
 	        }
@@ -202,5 +225,13 @@ public class TextPlayer implements VisualDisplay<String> {
         }
         return filePath.substring(lastIndex + 1);
     }
+	
+	
+
+	@Override
+	public @Nullable Image getDisplay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
